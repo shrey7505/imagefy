316ee72e805939a4f91deaf593f64513e7e8a3e3
Shrey Shah <shreyshahworld@gmail.com>
2025-08-08 16:01:12 +0530

    Update imageContriller.js

--- backend/controllers/imageContriller.js
+++ backend/controllers/imageContriller.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel.js";
+import userModel from "../models/usermodel.js";
 import FormData from "form-data";
 import axios from "axios";
 export const generateImage = async (req, res) => {
